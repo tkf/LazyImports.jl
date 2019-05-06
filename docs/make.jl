98e08198f1fee0b3d667b27621ebe10c3d792d8c
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-06 11:35:07 -0700

    Fix/simplify makedocs args

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,8 @@ using Documenter, LazyImports
 
 makedocs(;
     modules=[LazyImports],
-    format=Documenter.HTML(),
-    pages=[
-        "Home" => "index.md",
-    ],
-    repo="https://github.com/tkf/LazyImports.jl/blob/{commit}{path}#L{line}",
     sitename="LazyImports.jl",
     authors="Takafumi Arakaki",
-    assets=[],
 )
 
 deploydocs(;
